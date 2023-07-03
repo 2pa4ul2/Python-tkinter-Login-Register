313fa1f149a31a7a433dbe060a67db568199cae4
2pa4ul2 <pauladrian0224@gmail.com>
2023-07-03 20:29:24 +0800

    Updated the code

--- Register.py
+++ Register.py
@@ -30,19 +30,7 @@ def register():
 #create window
 window = ttk.Tk()
 window.title('Login System')# title the mainwindow
-
-window_width = 1280
-window_height = 720
-display_width = window.winfo_screenwidth()
-display_height = window.winfo_screenheight()
-
-left = int(display_width/2-window_width/2)
-top = int(display_height/2-window_height/2)
-window.geometry(f'{window_width}x{window_height}+{left}+{top}')
-
-window.configure(bg = '#0b0c10') #color
-window.minsize(1280,720) #set minsize
-window.resizable('false','false')
+window.geometry('1000x650')
 window.bind('<Escape>', lambda event: window.quit())#set escape as exit window
 
 
--- main.py
+++ main.py
@@ -31,46 +31,70 @@ def register():
 #create window
 window = ttk.Tk()
 window.title('Login System')# title the mainwindow
-
-window_width = 1280
-window_height = 720
-display_width = window.winfo_screenwidth()
-display_height = window.winfo_screenheight()
-
-left = int(display_width/2-window_width/2)
-top = int(display_height/2-window_height/2)
-window.geometry(f'{window_width}x{window_height}+{left}+{top}')
-
-window.configure(bg = '#0b0c10') #color
-window.minsize(1280,720) #set minsize
-window.resizable('false','false')
+window.geometry('1000x650')
 window.bind('<Escape>', lambda event: window.quit())#set escape as exit window
 
+#window_width = 1280
+#window_height = 720
+#display_width = window.winfo_screenwidth()
+#display_height = window.winfo_screenheight()
+
+#left = int(display_width/2-window_width/2)
+#top = int(display_height/2-window_height/2)
+#window.geometry(f'{window_width}x{window_height}+{left}+{top}')
+
+#window.configure(bg = '#0b0c10') #color
+#window.minsize(1000,720) #set minsize
+#window.resizable('false','false')
+
+
+
+#button = ttk.Style()
+
 #create of frame where the widgets will be placed
-frame1 = ttk.Frame( master = window,bg = '#1f2533', width=650)
+frame1 = ttk.Frame( master = window,bg = '#FFFFFF', width=400)
 frame1.pack(side='right', anchor='n', fill=ttk.BOTH, expand=ttk.TRUE)#place/display the frame
-frame2 = ttk.Frame( master = window,bg = '#84ceeb')
-frame2.pack(side='left', fill=ttk.BOTH, expand=ttk.YES)#place/display the frame
+frame2 = ttk.Frame( master = window,width=200, bg = '#6387f2')
+frame2.pack(side='left', fill=ttk.BOTH,expand=ttk.YES)#place/display the frame
 
+
+#frame 2
+image = Image.open("SELL.png")
+photo = ImageTk.PhotoImage(image)
+
+# Create a label to display the image
+label = ttk.Label(frame2, image=photo,bg = '#6387f2')
+label.place(x=100, y=80)
+
+start_mess = ttk.Label(frame2, text='Start your',bg = '#6387f2', fg='#ffffff', font=('montserrat', 20, 'bold'))
+with_mess = ttk.Label(frame2, text='Journey with us',bg = '#6387f2', fg='#ffffff', font=('montserrat', 20, 'bold'))
+create_mess = ttk.Label(frame2, text='create an account and Join our community',bg = '#6387f2', fg='#ffffff', font=('montserrat', 10))
+
+#place
+start_mess.place(x=50,y=300)
+with_mess.place(x=50,y=350)
+create_mess.place(x=50,y=400)
+
+#frame 1
 #creation of widgets
-login_label = ttk.Label(frame1, text='Login Account',bg = '#1f2533', fg='#ffffff', font=('montserrat', 40, 'bold'))
-entry_label = ttk.Label(frame1, text='Username',bg = '#1f2533', fg='#ffffff', font=('montserrat', 16))
-entry_field = ttk.Entry(frame1, font=('montserrat', 16,))
-pass_label = ttk.Label(frame1, text='Password',bg = '#1f2533', fg='#ffffff', font=('montserrat', 16))
-entry_passfield = ttk.Entry(frame1, show='*', font=('montserrat', 16))
-login_button = ttk.Button(frame1, text='   Login  ', bg = '#66fcf1', font=('montserrat', 16), command=login)
-reg_button = ttk.Button(frame1, text='Register', bg = '#fff', font=('montserrat', 16), command=register)
+login_label = ttk.Label(frame1, text='Welcome to Sellify',bg = '#FFFFFF', fg='#6387f2', font=('montserrat', 25, 'bold'))
+entry_label = ttk.Label(frame1, text='Username',bg = '#FFFFFF', fg='BLACK', font=('montserrat', 12))
+entry_field = ttk.Entry(frame1, width=25,font=('montserrat',12,),highlightcolor='#6387f2', highlightthickness=2)
+pass_label = ttk.Label(frame1, text='Password',bg = '#FFFFFF', fg='BLACK', font=('montserrat', 12))
+entry_passfield = ttk.Entry(frame1, show='*', width=25, font=('montserrat', 12),highlightcolor='#6387f2', highlightthickness=2)
+login_button = ttk.Button(frame1, text='   Login  ',width=27, bg = '#6387f2',foreground='#FFFFFF', font=('montserrat', 12),activebackground= '#4973f2',activeforeground='#a9a9a9', highlightbackground= 'BLACK',height=2,command=login)
+reg_button = ttk.Button(frame1, text='Register',foreground='#6387f2',highlightcolor='#ffffff',highlightthickness=3,highlightbackground='#ffffff', bg = '#ffffff',width=27, font=('montserrat', 12), command=register)
 
-entry_field.config()
+
 
 #placing/displaying all the widget
-login_label.pack(pady=80)
-entry_label.pack(pady=10)
-entry_field.pack()
-pass_label.pack(pady=10)
-entry_passfield.pack()
-login_button.pack(pady=20)
-reg_button.pack()
+login_label.place(x= 125,y=130)
+entry_label.place(x= 150,y=205)
+pass_label.place(x= 150,y=285)
+entry_field.place(x= 155,y=240)
+entry_passfield.place(x= 155,y=320)
+login_button.place(x= 155,y=380)
+reg_button.place(x= 155,y=450)
 
 window.mainloop() #looping of window so it would display
 
